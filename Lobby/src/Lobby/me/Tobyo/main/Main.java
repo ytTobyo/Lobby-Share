a7837b5198e4c252063375ea59c64609af2d94e4
ytTobyo <youtu@DESKTOP-PIIT3O8>
2018-05-28 17:35:56 +0200

    Main

---
+++ Lobby/src/Lobby/me/Tobyo/main/Main.java
@@ -0,0 +1,18 @@
+package Lobby.me.Tobyo.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+}
